5cb449e8ffe143f792c528d92cdc0f7527e94212
TobiasGoransson <tobiasgoransson1@gmail.com>
2023-12-05 11:43:45 +0100

    test3

---
+++ Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSFashion
+{
+    internal class Item
+    {
+        public string ItemName { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSFashion
+{
+    internal class Order
+    {
+        public int OrderNo { get; set; }
+        public string Username { get; set; }
+        public DateTime Placedtime  { get; set; }
+        List<Item> Items { get; set; }
+    }
+}
