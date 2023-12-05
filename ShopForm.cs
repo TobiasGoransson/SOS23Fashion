a245ac27f0799663445292358a86a9b41ea6e929
HugoAndersson99 <hugooanderssonn@gmail.com>
2023-12-05 11:34:18 +0100

    First Design

---
+++ ShopForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOSFashion
+{
+    public partial class ShopForm : Form
+    {
+        public ShopForm()
+        {
+            InitializeComponent();
+        }
+
+        private void capLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
