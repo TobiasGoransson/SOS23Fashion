a5b5f92c9c53ad45a8bf6b39a61f2de83ba96276
Faluuja2020 <abdirahman.omar@student.nbi-handelsakademin.se>
2023-12-12 10:59:08 +0100

    Checkout

---
+++ Checkout.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace SOSFashion
+{
+    partial class Checkout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            label8 = new Label();
+            comboBox2 = new ComboBox();
+            label9 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(326, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 29);
+            label1.TabIndex = 0;
+            label1.Text = "Checkout";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 20);
+            label2.TabIndex = 1;
+            label2.Text = "First name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(100, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(152, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(354, 89);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(152, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(270, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Last name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(354, 170);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(152, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(270, 173);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Phone";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(100, 170);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(152, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(16, 170);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Email";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(354, 244);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(152, 27);
+            textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(270, 247);
+            label6.Name = "label6";
+            label6.Size = new Size(64, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Zipcode";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(100, 244);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(152, 27);
+            textBox6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 247);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 20);
+            label7.TabIndex = 12;
+            label7.Text = "Adress";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(557, 93);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(203, 284);
+            listBox1.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(100, 319);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 327);
+            label8.Name = "label8";
+            label8.Size = new Size(68, 20);
+            label8.TabIndex = 15;
+            label8.Text = "Shipping";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(354, 319);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(270, 327);
+            label9.Name = "label9";
+            label9.Size = new Size(65, 20);
+            label9.TabIndex = 17;
+            label9.Text = "Payment";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(232, 383);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 43);
+            button1.TabIndex = 18;
+            button1.Text = "Confirm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Checkout
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label9);
+            Controls.Add(comboBox2);
+            Controls.Add(label8);
+            Controls.Add(comboBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Checkout";
+            Text = "Checkout";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Label label8;
+        private ComboBox comboBox2;
+        private Label label9;
+        private Button button1;
+    }
+}
